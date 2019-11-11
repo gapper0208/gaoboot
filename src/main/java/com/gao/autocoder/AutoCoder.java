cda902ad54ace7d51beafd77870d43cfb4743c2f
gao <123@qq.com>
2019-11-11 15:31:42 +0800

    test...

--- src/main/java/com/gao/autocoder/AutoCoder.java
+++ src/main/java/com/gao/autocoder/AutoCoder.java
@@ -6,5 +6,5 @@ import com.gao.metadata.MetaData;
 // 代码生成器接口
 public interface AutoCoder<T extends MetaData> {
 	// 根据元数据来生成代码
-	Code generateCode(T md);  // test...
+	Code generateCode(T md);  // test... 
 }
